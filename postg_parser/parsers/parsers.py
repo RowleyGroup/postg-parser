eb3aba87990e5895790a1385c9fc242ffa431c1b
mohebifar <netfars@gmail.com>
2016-10-31 18:52:41 -0230

    Add c8 and c10 coefficients

--- postg_parser/parsers/parsers.py
+++ postg_parser/parsers/parsers.py
@@ -17,13 +17,19 @@ def coefficients_parser(input):
     number_of_atoms = int(sqrt(1 + 8 * len(lines)) - 1) / 2
 
     c6 = matrix(zeros(shape=(number_of_atoms, number_of_atoms)))
+    c8 = matrix(zeros(shape=(number_of_atoms, number_of_atoms)))
+    c10 = matrix(zeros(shape=(number_of_atoms, number_of_atoms)))
+    rvdw = matrix(zeros(shape=(number_of_atoms, number_of_atoms)))
 
     for line in lines:
         matches = search(coefficients_pattern, line).groups()
         numbers = map(lambda x: float(x) if '.' in x else int(x), matches)
         c6.itemset((numbers[0] - 1, numbers[1] - 1), numbers[3])
+        c8.itemset((numbers[0] - 1, numbers[1] - 1), numbers[4])
+        c10.itemset((numbers[0] - 1, numbers[1] - 1), numbers[5])
+        rvdw.itemset((numbers[0] - 1, numbers[1] - 1), numbers[7])
 
-    return Coefficients(c6=c6)
+    return Coefficients(c6=c6, c8=c8, c10=c10, rvdw=rvdw)
 
 
 def atoms_positions_parser(input):
